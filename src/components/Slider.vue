8090bc0dd924f84fbf5302d5081cd6d2a1e90c3b
William Hood <williamhood@Williams-MacBook-Pro-2.local>
2021-07-08 20:52:32 -0400

    Started building Slider component

---
+++ src/components/Slider.vue
@@ -0,0 +1,25 @@
+<template>
+	<Swiper>
+		
+	</Swiper>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+export default defineComponent({
+	name: 'Slider',
+	components: {
+		Swiper,
+		SwiperSlide
+	}
+})
+</script>
